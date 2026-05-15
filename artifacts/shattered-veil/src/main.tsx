368b8d97e68af8d3b622064f642b134da95e85a7
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-05-15 10:12:10 -0400

    Start P4 runtime movement bridge

--- artifacts/shattered-veil/src/main.tsx
+++ artifacts/shattered-veil/src/main.tsx
@@ -5,9 +5,11 @@ import "./battle/actionEconomy.css";
 import "./battle/battleControlRailLayout.css";
 import "./battle/battleControlRailFinalPolish.css";
 import { startBattleActionEconomyBridge } from "./battle/actionEconomyBridge";
+import { startP4RuntimeBridge } from "./battle/p4RuntimeBridge";
 import { startCreateInlineInnateTitlePolish } from "./createInlineInnateTitle";
 
 startBattleActionEconomyBridge();
+startP4RuntimeBridge();
 startCreateInlineInnateTitlePolish();
 
 createRoot(document.getElementById("root")!).render(<App />);
