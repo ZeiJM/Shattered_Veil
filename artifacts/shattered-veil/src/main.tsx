9ed9f5ff896d73cf3fa72e0b85570b8cd58bcc26
ZeiJM <nik.mohammed@gmail.com>
2026-05-14 20:36:11 -0500

    Start create and map UI polish bridge

--- artifacts/shattered-veil/src/main.tsx
+++ artifacts/shattered-veil/src/main.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import "./battle/actionEconomy.css";
 import "./battle/battleControlRailLayout.css";
 import { startBattleActionEconomyBridge } from "./battle/actionEconomyBridge";
+import { startCreateInlineInnateTitlePolish } from "./createInlineInnateTitle";
 
 startBattleActionEconomyBridge();
+startCreateInlineInnateTitlePolish();
 
 createRoot(document.getElementById("root")!).render(<App />);
