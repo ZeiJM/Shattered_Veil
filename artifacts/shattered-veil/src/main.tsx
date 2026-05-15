cb0d1d2c94c242b06382df40033258a965f4036d
ZeiJM <nik.mohammed@gmail.com>
2026-05-14 19:35:39 -0500

    Import battle control rail stylesheet

--- artifacts/shattered-veil/src/main.tsx
+++ artifacts/shattered-veil/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./battle/actionEconomy.css";
+import "./battle/battleControlRailLayout.css";
 import { startBattleActionEconomyBridge } from "./battle/actionEconomyBridge";
 
 startBattleActionEconomyBridge();
