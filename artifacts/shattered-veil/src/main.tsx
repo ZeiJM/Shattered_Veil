a09bf09853c9c429c81b689ded4df2d8e7dec156
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-05-16 12:26:00 -0400

    Start native P4 runtime sync

--- artifacts/shattered-veil/src/main.tsx
+++ artifacts/shattered-veil/src/main.tsx
@@ -6,10 +6,12 @@ import "./battle/battleControlRailLayout.css";
 import "./battle/battleControlRailFinalPolish.css";
 import { startBattleActionEconomyBridge } from "./battle/actionEconomyBridge";
 import { startP4RuntimeBridge } from "./battle/p4RuntimeBridge";
+import { startP4NativeRuntimeSync } from "./battle/p4NativeRuntimeSync";
 import { startCreateInlineInnateTitlePolish } from "./createInlineInnateTitle";
 
 startBattleActionEconomyBridge();
 startP4RuntimeBridge();
+startP4NativeRuntimeSync();
 startCreateInlineInnateTitlePolish();
 
 createRoot(document.getElementById("root")!).render(<App />);
