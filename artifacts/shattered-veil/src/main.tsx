1b2c8862c078fb3c1c50529e472aa00069af8da6
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-05-17 14:24:19 -0400

    Start P4 integration facade

--- artifacts/shattered-veil/src/main.tsx
+++ artifacts/shattered-veil/src/main.tsx
@@ -6,10 +6,12 @@ import "./battle/battleControlRailLayout.css";
 import "./battle/battleControlRailFinalPolish.css";
 import { startBattleActionEconomyBridge } from "./battle/actionEconomyBridge";
 import { startP4RuntimeBridge } from "./battle/p4RuntimeBridge";
+import { startP4IntegrationFacade } from "./battle/p4IntegrationFacade";
 import { startCreateInlineInnateTitlePolish } from "./createInlineInnateTitle";
 
 startBattleActionEconomyBridge();
 startP4RuntimeBridge();
+startP4IntegrationFacade();
 startCreateInlineInnateTitlePolish();
 
 createRoot(document.getElementById("root")!).render(<App />);
